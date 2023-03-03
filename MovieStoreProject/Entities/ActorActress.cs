270bed8f2f1acae65fc7a677a71ffa3304baac80
Onur ÖZDEMİR <ozdmronurr@gmail.com>
2023-03-03 17:17:27 +0300

    Add project files.

---
+++ MovieStoreProject/Entities/ActorActress.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieStoreWebApi.Entities
+{
+    public class ActorActress
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        //public int MovieId { get; set; }
+        //public Movie Movie { get; set; } // rol aldığı filmler
+
+    }
+}
